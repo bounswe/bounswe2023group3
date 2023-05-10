beba47e49e853c7909695cbd3497df42388fba48
Simar <simar.ahmetkahya@gmail.com>
2023-05-10 21:26:02 +0300

    added the path to public_transportation_app to my section in the home screen and made necessary changes in the app urls

--- practice-app/predictionPoll/predictionPoll/urls.py
+++ practice-app/predictionPoll/predictionPoll/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('transportation/', include('public_transport_app.urls'))
 ]
