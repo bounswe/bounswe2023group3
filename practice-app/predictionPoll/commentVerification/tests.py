3a9fb27248ff3fe4b19c604b268d5652c6d84b5b
farukyld <120921965+farukyld@users.noreply.github.com>
2023-05-12 01:37:21 +0300

    Update tests.py
    added test cases to the file

--- practice-app/predictionPoll/commentVerification/tests.py
+++ practice-app/predictionPoll/commentVerification/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import CommentValidationHistory
 
-# Create your tests here.
+class CommentVerificationTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_moderate_comment_get_request(self):
+        response = self.client.get(reverse('moderate_comment'))
+        self.assertEqual(response.status_code, 200)
+        # going to check for the content maybe
+
+    def test_moderate_comment_post_request(self):
+        data = {'comment': 'Test comment'}
+        response = self.client.post(reverse('moderate_comment'), data)
+        self.assertEqual(response.status_code, 200)
+        # going to check for the content maybe
+
+        # check if last comment is added!!
+        comment_exists = CommentValidationHistory.objects.filter(comment_body=data['comment']).exists()
+        self.assertTrue(comment_exists)
+
+    def test_comment_history(self):
+        response = self.client.get(reverse('comment_history'))
+        self.assertEqual(response.status_code, 200)
+        # going to check for the content maybe
+
+        self.assertContains(response, '<html>')
+        self.assertContains(response, 'Comment History')
