670b96f5c1ecb2f1abd6bf58d58d35f8b160261d
BatuhanIlhan <mbatuhanilhan@gmail.com>
2023-05-11 17:29:13 +0300

    connect views to urls

--- practice-app/predictionPoll/universities/urls.py
+++ practice-app/predictionPoll/universities/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('listall', views.getUniversitiesInTurkey),
+    path('list', views.listUniversities),
+    path('add', views.createUniversityWithReview),
+    path('', views.display),
 ]
