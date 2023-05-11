ce92ffc7ef13f045a5667bf5953bc32d8f9e0eae
farukyld <120921965+farukyld@users.noreply.github.com>
2023-05-12 01:35:45 +0300

    Update urls.py
    add names of the views to be able to run tests correctly

--- practice-app/predictionPoll/commentVerification/urls.py
+++ practice-app/predictionPoll/commentVerification/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route="check-comment-validity/",view=views.moderate_comment),
-    path(route="list-comment-history/",view=views.comment_history),
+    path(route="check-comment-validity/",view=views.moderate_comment,name="moderate_comment"),
+    path(route="list-comment-history/",view=views.comment_history,name="comment_history"),
     path(route="",view=views.display)
 ]
