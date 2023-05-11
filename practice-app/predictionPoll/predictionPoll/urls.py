1405bd2b0798298f9665d2e39453e2a07ebcbb8b
BatuhanIlhan <mbatuhanilhan@gmail.com>
2023-05-11 17:22:10 +0300

    url and setting configurations

--- practice-app/predictionPoll/predictionPoll/urls.py
+++ practice-app/predictionPoll/predictionPoll/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('universities/', include('universities.urls')),
     path('', include('home.urls')),
 ]
