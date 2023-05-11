39b98c4cb17601fd6173a17540213ec4bf39a608
beyza-akcinar <93255363+beyza-akcinar@users.noreply.github.com>
2023-05-12 01:39:21 +0300

    Weather_app is added

--- practice-app/predictionPoll/predictionPoll/urls.py
+++ practice-app/predictionPoll/predictionPoll/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('weather/', include('weather_app.urls'))
 ]
