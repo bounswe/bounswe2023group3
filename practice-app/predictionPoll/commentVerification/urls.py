2eef101b9a175b1e0972c02bdace8fb25d1b2353
farukyld <120921965+farukyld@users.noreply.github.com>
2023-05-12 00:00:03 +0300

    Update urls.py adding url of the view "display"

--- practice-app/predictionPoll/commentVerification/urls.py
+++ practice-app/predictionPoll/commentVerification/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path(route="check-comment-validity/",view=views.moderate_comment),
-    path(route="list-comment-history/",view=views.comment_history)
+    path(route="list-comment-history/",view=views.comment_history),
+    path(route="",view=views.display)
 ]
