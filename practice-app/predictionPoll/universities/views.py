18df94f6e8f08246d5360f25fba7c3360851588c
BatuhanIlhan <mbatuhanilhan@gmail.com>
2023-05-12 17:39:09 +0300

    add swagger

--- practice-app/predictionPoll/universities/views.py
+++ practice-app/predictionPoll/universities/views.py
@@ -2,6 +2,10 @@ import json
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import api_view
+
 from .models import Review, University
 import requests
 from .forms import CreateUniversityWithReviewForm
@@ -12,11 +16,43 @@ def display(request):
     return render(request, 'temp.html')
 
 
+@swagger_auto_schema(
+    method='GET',
+    operation_summary='Get the all universities from an external api',
+    responses={
+        200: 'A list of the universities in JSON format'
+    }
+)
+@api_view(['GET'])
 def getUniversitiesInTurkey(request):
     response = requests.get("http://universities.hipolabs.com/search?country=Turkey")
     return JsonResponse(json.loads(response.content), safe=False)
 
 
+@swagger_auto_schema(
+    method='POST',
+    operation_summary='Create a new university and add a review',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'name': openapi.Schema(type=openapi.TYPE_STRING),
+            'web_page': openapi.Schema(type=openapi.TYPE_STRING),
+            'review': openapi.Schema(type=openapi.TYPE_STRING)
+        },
+        required=['name', 'email', 'feedback']
+    ),
+    responses={
+        200: openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'name': openapi.Schema(type=openapi.TYPE_STRING),
+                'web_page': openapi.Schema(type=openapi.TYPE_STRING),
+                'review': openapi.Schema(type=openapi.TYPE_STRING)
+            }),
+        400: 'Invalid input'
+    }
+)
+@api_view(['POST'])
 @csrf_exempt
 def createUniversityWithReview(request):
     if request.method == "POST":
@@ -34,6 +70,14 @@ def createUniversityWithReview(request):
         return HttpResponse(status=405)
 
 
+@swagger_auto_schema(
+    method='GET',
+    operation_summary='Get the all universities in the DB',
+    responses={
+        200: 'A list of the universities in JSON format'
+    }
+)
+@api_view(['GET'])
 def listUniversities(request):
     universities = University.objects.all()
     context = []
