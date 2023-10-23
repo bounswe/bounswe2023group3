e3ce9e811b15b3faaf3f813ee341fc497c52c98b
batuhancetin1 <batuhan.cetin@harpeon.com>
2023-10-23 19:44:24 +0300

    swagger added

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Boun SWE 2023 Group 3 API')
+    .setDescription('Boun SWE 2023 Group 3 API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
