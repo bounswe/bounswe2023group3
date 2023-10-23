bbc89650f3ea118ef3bc41ad4521958d90b99081
Alp <alp.tuna.453@gmail.com>
2023-10-23 21:05:39 +0300

    Implement healtcheck endpoint

--- app/backend/src/app.service.ts
+++ app/backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  healtcheck(): string {
+    return 'App is up and runnning';
   }
 }
