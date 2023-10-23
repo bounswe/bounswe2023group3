006bba30fa5483737ceb979268faeb90f24be88b
Alp <alp.tuna.453@gmail.com>
2023-10-23 21:56:34 +0300

    Implement create, get and delete operations for users

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -2,14 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
-
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(helmet({
-    contentSecurityPolicy: false,
-  }));
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Boun SWE 2023 Group 3 API')
     .setDescription('Boun SWE 2023 Group 3 API Documentation')
@@ -18,6 +20,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
