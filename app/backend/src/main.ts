ed2fcfe8f535eb029a1edc3f9970f1ac77983b38
Alp <alp.tuna.453@gmail.com>
2023-10-24 15:47:47 +0300

    Fixed minor bugs

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
