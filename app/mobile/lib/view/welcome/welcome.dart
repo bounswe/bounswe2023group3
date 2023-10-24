2af7cd8e52cab0512b04a49f95f7d5b3d6f8886e
faruk.yld <123uzunince123@gmail.com>
2023-10-24 09:42:55 +0300

    added initial version of welcome page

---
+++ app/mobile/lib/view/welcome/welcome.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 40),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Text(
+                'Log in',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to Sign Up Page
+                print("welcome.WelcomeScreen.build.Scaffold.Center.Column."
+                    "ElevatedButton: navigate to sign up page");
+              },
+              child: Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 20),
+              ),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 34),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                // Continue as Guest
+              },
+              child: Text('Continue as Guest'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
