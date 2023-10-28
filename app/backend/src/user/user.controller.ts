28e7c6fffb5f163e2cde975dddb55f0b7b0007eb
Alp <alp.tuna.453@gmail.com>
2023-10-29 00:02:48 +0300

    Delete unneccessary post user endpoint

--- app/backend/src/user/user.controller.ts
+++ app/backend/src/user/user.controller.ts
@@ -1,24 +1,11 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { ApiBearerAuth } from '@nestjs/swagger';
 
 @ApiBearerAuth()
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.createUser(createUserDto);
-  }
 
   @Get()
   findAll() {
