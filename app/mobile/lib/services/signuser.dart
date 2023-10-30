375953056dba9c329460b1645ac11ebafd8cce39
berkecaliskan <berkecaliskan2001@gmail.com>
2023-10-30 21:17:49 +0300

    username field added to signup

--- app/mobile/lib/services/signuser.dart
+++ app/mobile/lib/services/signuser.dart
@@ -5,7 +5,7 @@ import 'package:mobile_app/services/apiService.dart';
 
 class SignUser {
 
-  Future<Response> sign(String username, String password) async {
+  Future<Response> sign(String email, String username, String password) async {
     const String signEndpoint = '/auth/sign'; // Replace with your login endpoint
 
     final Map<String, String> data = {
