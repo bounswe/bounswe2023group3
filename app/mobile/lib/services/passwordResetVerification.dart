6e59ea75fcfead36d18defbbb0ea7ef505dc107c
Baturhan Akbulut <tahaakbulut@Baturhans-M1-3.local>
2023-10-31 21:15:21 +0300

    Password reset is connected.

--- app/mobile/lib/services/passwordResetVerification.dart
+++ app/mobile/lib/services/passwordResetVerification.dart
@@ -4,7 +4,7 @@ import 'package:mobile_app/services/apiService.dart';
 
 class PasswordResetVerification {
 
-  Future<Response> submitPass(int OTP, String id, String password) async {
+  Future<Response> submitPass(int OTP, String email, String password) async {
     const String resetPasswordEndpoint =
         '/auth/reset-password';
 
@@ -14,7 +14,7 @@ class PasswordResetVerification {
       final Response response = await ApiService.dio.post(
         resetPasswordEndpoint,
         data: {'resetPasswordToken': OTP,
-          'id': id,
+          'email': email,
           'password': password,},
       );
 
