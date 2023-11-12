c36d5d1bdcb8faf3fd8acf2f640666a071529059
Alp <alp.tuna.453@gmail.com>
2023-11-12 18:20:54 +0300

    Add create poll function

--- app/backend/src/poll/poll.service.ts
+++ app/backend/src/poll/poll.service.ts
@@ -10,10 +10,53 @@ import { CreatePollDto } from './dto/create-poll.dto';
 export class PollService {
   constructor(
     @InjectRepository(Poll) private readonly pollRepository: Repository<Poll>,
-    @InjectRepository(Option) private readonly optionRepository: Repository<Option>,
+    @InjectRepository(Option)
+    private readonly optionRepository: Repository<Option>,
     @InjectRepository(Tag) private readonly tagRepository: Repository<Tag>,
   ) {}
 
+  public async createPoll(createPollDto: CreatePollDto): Promise<Poll> {
+    const poll = new Poll();
+    poll.question = createPollDto.question;
+    poll.creator = createPollDto.creator;
+    poll.moderator = 'moderator';
+
+    // Save the poll first to generate the ID
+    const savedPoll = await this.pollRepository.save(poll);
+
+    // Create and associate options with the saved poll
+    const options = createPollDto.options.map((option) => {
+      const newOption = new Option();
+      newOption.answer = option;
+      newOption.poll = savedPoll;
+      return newOption;
+    });
+
+    // Save the options
+    await this.optionRepository.save(options);
+
+    // If you want to associate tags with the poll, you can do it here
+    // Assuming createPollDto.tags is an array of tag names
+    const tags = await Promise.all(
+      createPollDto.tags.map(async (tagName) => {
+        let tag = await this.tagRepository.findOneBy({ name: tagName });
+
+        if (!tag) {
+          tag = new Tag();
+          tag.name = tagName;
+          await this.tagRepository.save(tag);
+        }
+
+        return tag;
+      }),
+    );
+
+    // Associate tags with the poll
+    savedPoll.tags = tags;
+
+    // Save the poll again to update the associations
+    return await this.pollRepository.save(savedPoll);
+  }
 
   public async findAll(): Promise<Poll[]> {
     return await this.pollRepository.find({
@@ -25,7 +68,7 @@ export class PollService {
     return await this.pollRepository.findOne({
       where: { id },
       relations: ['options', 'tags', 'creator'],
-      });
+    });
   }
 
   public async removeById(id: string): Promise<void> {
