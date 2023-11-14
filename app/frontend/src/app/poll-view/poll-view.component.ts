9d4fbbe8d8fbaeb006a6a785ad9d9a54a9b40daf
beyza-akcinar <beyza.akcinar@hotmail.com>
2023-11-14 21:29:42 +0300

    FE | Header Added

---
+++ app/frontend/src/app/poll-view/poll-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poll-view',
+  templateUrl: './poll-view.component.html',
+  styleUrls: ['./poll-view.component.css']
+})
+export class PollViewComponent {
+
+}
