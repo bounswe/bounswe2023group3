6f6d4a301076db7e13d1a72262fa2e26b99ad03d
batuhancetin1 <batuhan.cetin@harpeon.com>
2023-11-19 16:30:50 +0300

    Fetch unapproved polls endpoint added

--- app/backend/src/moderator/moderator.controller.ts
+++ app/backend/src/moderator/moderator.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ModeratorService } from './moderator.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @Controller('moderator')
 export class ModeratorController {
   constructor(private readonly moderatorService: ModeratorService) {}
+
+  @Get()
+  fetchUnapprovedPolls() {
+    return this.moderatorService.fetchUnapprovedPolls();
+  }
 }
--- app/backend/src/moderator/moderator.service.ts
+++ app/backend/src/moderator/moderator.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Poll } from '../poll/entities/poll.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ModeratorService {}
+export class ModeratorService {
+    constructor(
+        @InjectRepository(Poll) private readonly pollRepository: Repository<Poll>,
+    ) {}
+
+    public async fetchUnapprovedPolls(): Promise<Poll[]> {
+        return await this.pollRepository.find({
+            where: {
+                is_approved_by_moderator: false
+            },
+            relations: ['options', 'tags', 'creator'],
+        });
+    }
+}
