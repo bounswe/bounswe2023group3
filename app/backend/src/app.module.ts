1d91205fefe50e5f32f03e6cc788e7da2b3e39cb
BatuhanIlhan <mbatuhanilha@gmail.com>
2023-11-21 16:14:09 +0300

    be-315: Fix db address

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -19,7 +19,7 @@ import { Moderator } from './moderator/entities/moderator.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: '172.17.0.2',
       port: 5432,
       password: 'password',
       username: 'postgres',
