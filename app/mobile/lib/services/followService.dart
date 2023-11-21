43eed8978ef98d9e6f99982f7090443b66d3d20d
faruk.yld <123uzunince123@gmail.com>
2023-11-21 13:02:20 +0300

    add mock follow service

---
+++ app/mobile/lib/services/followService.dart
@@ -0,0 +1,8 @@
+class FollowService {
+  static Future<bool> isFollowing(String followerId, String followedId) async {
+    return false;
+  }
+
+  static Future<void> follow(String followerId, String followedId) async {}
+  static Future<void> unfollow(String followerId, String followedId) async {}
+}
