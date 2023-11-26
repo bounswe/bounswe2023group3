8b26f8c3137d3a9ed72608faaaba418ca1f4c6dd
faruk.yld <123uzunince123@gmail.com>
2023-11-26 16:59:04 +0300

    remove mock list

--- app/mobile/lib/services/profilePagePollsService.dart
+++ app/mobile/lib/services/profilePagePollsService.dart
@@ -1,60 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:mobile_app/models/comment.dart';
+import 'package:dio/dio.dart';
 import 'package:mobile_app/models/pollInfo.dart';
+import 'package:mobile_app/services/apiService.dart';
 
 class ProfilePagePollsService {
   static Future<List<PollInfo>> getLikedPolls(String username) async {
-    await Future.delayed(const Duration(seconds: 1));
-    return mockList;
+    // TODO make filtering
+    // TODO handle errors
+    String pollsEndpoint = "/poll";
+    Response response = await ApiService.dio.get(pollsEndpoint);
+    var polls = List.from(response.data).map((e) => PollInfo.fromJson(e));
+    return polls.toList();
   }
 
   static Future<List<PollInfo>> getVotedPolls(String username) async {
-    await Future.delayed(const Duration(seconds: 1));
-    return mockList;
+    // TODO make filtering
+    // TODO handle errors
+    String pollsEndpoint = "/poll";
+    Response response = await ApiService.dio.get(pollsEndpoint);
+    var polls = List.from(response.data).map((e) => PollInfo.fromJson(e));
+    return polls.toList();
   }
 
-  static List<PollInfo> mockList = [
-    PollInfo(
-        userName: "berkecaliskan",
-        userUsername: "@berke",
-        profilePictureUrl:
-            "https://yt3.googleusercontent.com/bWL_Q46Ob6MxdYmMP7hWaox_pFLja8uh1iI02F9CtV-eaeR409j3xfWLG0GbmTzVEwX5R38ur2k=s900-c-k-c0x00ffffff-no-rj",
-        postTitle: "Who will win the Super Cup?",
-        tags: ["Sport"],
-        tagColors: [Colors.blue],
-        voteCount: 34,
-        postOptions: ["Fenerbahçe", "Galatasaray"],
-        likeCount: 13,
-        dateTime: DateTime.now().add(const Duration(days: 50)),
-        comments: [
-          CommentData(
-              user: "real_elijah", commentText: "mazinde bir tarih yatar")
-        ]),
-    PollInfo(
-        userName: "elijahwood",
-        userUsername: "@real_elijah",
-        profilePictureUrl:
-            "https://m.media-amazon.com/images/M/MV5BMTM0NDIxMzQ5OF5BMl5BanBnXkFtZTcwNzAyNTA4Nw@@._V1_FMjpg_UX1000_.jpg",
-        postTitle: "What should I do with the Ring?",
-        tags: ["Life"],
-        tagColors: [Colors.red],
-        voteCount: 1347,
-        postOptions: ["Destroy it", "Wear it", "Give it to Selda Bağcan"],
-        likeCount: 673,
-        dateTime: DateTime.now().add(const Duration(days: 30)),
-        comments: []),
-    PollInfo(
-        userName: "kabakhaber",
-        userUsername: "@kbkhbr",
-        profilePictureUrl:
-            "https://static.ticimax.cloud/3140/uploads/urunresimleri/buyuk/b7387ec8-469f-4826-a914-094d62362971.jpg",
-        postTitle: "Who will win the elections?",
-        tags: ["News"],
-        tagColors: [Colors.green],
-        voteCount: 534,
-        postOptions: ["Donald Trump", "Joe Biden", "Ye"],
-        likeCount: 297,
-        dateTime: DateTime.now().add(const Duration(days: 90)),
-        comments: []),
-  ];
+  static Future<List<PollInfo>> getCreatedPolls(String username) async {
+    await Future.delayed(const Duration(seconds: 1));
+    return [];
+  }
 }
