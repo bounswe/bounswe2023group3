944387ad79045a316a1e979e7a8ff49b2a392bb1
Simar <simar.ahmetkahya@gmail.com>
2023-11-26 16:34:25 +0300

    added necessary backend connections and modified code accordingly

--- app/mobile/lib/services/moderatorPollDecision.dart
+++ app/mobile/lib/services/moderatorPollDecision.dart
@@ -4,9 +4,9 @@ import 'package:mobile_app/services/apiService.dart';
 
 class ModeratorPollDecision {
 
-  Future<Response> answerPoll(bool isApproved) async {
-    const String pollAnswerEndpoint =
-        '/moderator/pollApprove';
+  Future<Response> answerPoll(bool isApproved, String id) async {
+    String pollAnswerEndpoint =
+        '/moderator/pollApprove/$id';
 
 
 
@@ -14,7 +14,7 @@ class ModeratorPollDecision {
     try {
       final Response response = await ApiService.dio.post(
         pollAnswerEndpoint,
-        data: {'approve': isApproved,
+        data: {'approveStatus': isApproved,
           },
       );
 
