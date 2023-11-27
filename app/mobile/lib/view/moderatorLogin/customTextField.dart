2575e26aae7374723e9d782a2fea28983665a1ea
Baturhan Akbulut <tahaakbulut@Baturhans-M1-3.local>
2023-11-27 03:48:07 +0300

    Moderator Login page is implemented.

---
+++ app/mobile/lib/view/moderatorLogin/customTextField.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app/view/constants.dart';
+
+
+class CustomTextField extends StatelessWidget {
+  final String labelText;
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final bool obscureText;
+  final Function(String) onChanged;
+  final String errorText;
+
+  const CustomTextField({
+    Key? key,
+    required this.labelText,
+    required this.controller,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+    required this.onChanged,
+    required this.errorText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            color: whitish,
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(
+              color: navy,
+              width: 3.0,
+            ),
+          ),
+          child: TextField(
+            controller: controller,
+            keyboardType: keyboardType,
+            obscureText: obscureText,
+            onChanged: onChanged,
+            decoration: InputDecoration(
+              hintText: labelText,
+              border: InputBorder.none,
+              labelStyle: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0, left: 12.0),
+          child: Text(
+            errorText,
+            style: TextStyle(color: Colors.red[900]),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ app/mobile/lib/view/moderatorLogin/moderatorLoginScreen.dart
@@ -0,0 +1,174 @@
+import 'package:dio/dio.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_app/services/moderatorAuthService.dart';
+import 'package:mobile_app/view/state.dart';
+import '../constants.dart';
+import 'customTextField.dart';
+
+class ModeratorLoginScreen extends StatefulWidget {
+  const ModeratorLoginScreen({super.key});
+
+  @override
+  State<ModeratorLoginScreen> createState() => _ModeratorLoginScreenState();
+}
+
+class _ModeratorLoginScreenState extends State<ModeratorLoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  bool isEmailValid = true; // Track email validation
+  bool isPasswordValid = true; // Add more validation as needed for password
+  bool emptyEmail = false;
+  bool emptyPassword = false;
+
+  void validateEmail(String email) {
+    setState(() {
+      emptyPassword = false;
+      emptyEmail = false;
+      if (email.isEmpty) {
+        isEmailValid = true;
+      } else {
+        isEmailValid = EmailValidator.validate(email);
+      }
+    });
+  }
+
+  // Add more validation logic for password as needed
+  void validatePassword(String password) {
+    setState(() {
+      emptyPassword = false;
+      emptyEmail = false;
+      if (password.isEmpty) {
+        isPasswordValid = true;
+      } else {
+        isPasswordValid = password.length >=
+            6; // For example, password should be at least 6 characters
+      }
+    });
+  }
+
+  void login() async {
+    String email = emailController.text;
+    String password = passwordController.text;
+    if (email.isEmpty) {
+      setState(() {
+        emptyEmail = true;
+      });
+    }
+    if (password.isEmpty) {
+      setState(() {
+        emptyPassword = true;
+      });
+    }
+    if (emptyEmail || emptyPassword) {
+      return;
+    }
+    // Perform login validation and navigate to the next screen if successful
+    ModeratorAuthService moderatorAuthService = ModeratorAuthService();
+    try {
+      Response response = await moderatorAuthService.login(email, password);
+      print(response);
+      if (response.statusCode == 201) {
+        moderatorAuthService.saveToken(response.data['access_token']);
+        /*try {
+          Response loggedInUserData = await ModeratorApiService.dio.get('/auth/me');
+          if (loggedInUserData.statusCode == 200) {
+            AppState.loggedInUserId = loggedInUserData.data['id'];
+            print("loginScreen.login: ${AppState.loggedInUserId}");
+          } else {
+            if (!context.mounted) return;
+            showErrorMessage(context);
+          }
+        } catch (e) {
+          print("!!! loginScreen.dart::_LoginScreenState.login $e");
+          // showErrorMessage(context);
+        }*/
+        if (!context.mounted) return;
+        Navigator.pushNamed(context, '/moderatorHome');
+      } else {
+        if (!context.mounted) return;
+        showErrorMessage(context);
+      }
+    } catch (e) {
+      showErrorMessage(context);
+    }
+  }
+
+  void showErrorMessage(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Email or password is incorrect'),
+      ),
+    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomTextField(
+                  labelText: 'Email',
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: validateEmail,
+                  errorText: isEmailValid ? "" : 'Enter a valid email',
+                ),
+                const SizedBox(height: 16),
+                CustomTextField(
+                  labelText: 'Password',
+                  controller: passwordController,
+                  obscureText: true,
+                  onChanged: validatePassword,
+                  errorText: isPasswordValid
+                      ? ""
+                      : 'Password must be at least 6 characters',
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  // onPressed: isEmailValid && isPasswordValid ? login : null,
+                  onPressed: login,
+                  child: const Text('Login'),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Text(
+                        emptyEmail ? 'Email cannot be empty' : '',
+                        style: TextStyle(
+                          color: Colors.red[900],
+                        ),
+                      ),
+                      Text(
+                        emptyPassword ? 'Password cannot be empty' : '',
+                        style: TextStyle(
+                          color: Colors.red[900],
+                        ),
+                      ),
+
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
