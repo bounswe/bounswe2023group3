70e1ffd9586a130132c233683f3be429ca0946c7
berkecaliskan <berkecaliskan2001@gmail.com>
2023-12-11 19:22:22 +0300

    like/dislike mechanism added

--- app/mobile/lib/services/pollViewHomePageLike.dart
+++ app/mobile/lib/services/pollViewHomePageLike.dart
@@ -4,16 +4,39 @@ import 'package:dio/dio.dart';
 import 'package:mobile_app/services/apiService.dart';
 
 class PollViewHomePageLike{
-  void like(String pollId) async {
+  Future<bool> like(String pollId) async {
     final String likeEndpoint = '/like/$pollId';
 
     try {
       final Response response = await ApiService.dio.post(
         likeEndpoint,
       );
+      print(response.statusCode);
+
+      if (response.statusCode == 201) {
+        return true; // Like successful
+      }
     } catch (e) {
       print('Error: $e');
       rethrow;
     }
+    return false;
+  }
+  Future<bool> unlike(String pollId) async {
+    final String likeEndpoint = '/like/$pollId';
+
+    try {
+      final Response response = await ApiService.dio.delete(
+        likeEndpoint,
+      );
+      print(response.statusCode);
+      if (response.statusCode == 200) {
+        return true; // Like successful
+      }
+    } catch (e) {
+      print('Error: $e');
+      rethrow;
+    }
+    return false;
   }
 }
