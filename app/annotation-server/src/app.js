41525440ce523ea7f548a4bfefe43078cb0b6a63
Alp <alp.tuna.453@gmail.com>
2023-12-16 00:32:12 +0300

    Update hello message

--- app/annotation-server/src/app.js
+++ app/annotation-server/src/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+  res.send("Hello from Annotation Server!");
 });
 
 app.listen(port, async () => {
