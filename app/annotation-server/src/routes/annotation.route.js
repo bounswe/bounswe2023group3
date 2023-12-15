e2af17a947c222a5fb1cfdbd808ef467cdd4c4cc
Alp <alp.tuna.453@gmail.com>
2023-12-16 01:40:03 +0300

    Add validation middleware

--- app/annotation-server/src/routes/annotation.route.js
+++ app/annotation-server/src/routes/annotation.route.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const annotationController = require("../controllers/annotation.controller");
+const validateResource = require("../middlewares/validateResource");
+const annotationSchemas = require("../schemas/annotation.schemas");
 
 const router = express.Router();
 
-router.get("/annotation/:id", annotationController.getAnnotation);
+router.get(
+  "/annotation/:id",
+  validateResource(annotationSchemas.getAnnotationSchema),
+  annotationController.getAnnotation
+);
 router.get("/annotation", annotationController.getAnnotations);
-router.post("/annotation", annotationController.createAnnotation);
-router.patch("/annotation/:id", annotationController.updateAnnotation);
-router.delete("/annotation/:id", annotationController.deleteAnnotation);
+router.post(
+  "/annotation",
+  validateResource(annotationSchemas.createAnnotationSchema),
+  annotationController.createAnnotation
+);
+router.patch(
+  "/annotation/:id",
+  validateResource(annotationSchemas.updateAnnotationSchema),
+  annotationController.updateAnnotation
+);
+router.delete(
+  "/annotation/:id",
+  validateResource(annotationSchemas.deleteAnnotationSchema),
+  annotationController.deleteAnnotation
+);
 
 module.exports = router;
