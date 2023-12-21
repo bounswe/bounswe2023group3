dc01cfc6b00f74caca43dafa150b2b7adfc57484
batuhancetin <esbatuhanes@gmail.com>
2023-12-21 18:24:44 +0300

    moderator guard and uniquness is added

--- app/backend/src/tag/tag.module.ts
+++ app/backend/src/tag/tag.module.ts
@@ -3,11 +3,17 @@ import { TagService } from './tag.service';
 import { TagController } from './tag.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tag } from './entities/tag.entity';
+import { ModeratorService } from '../moderator/moderator.service';
+import { Moderator } from '../moderator/entities/moderator.entity';
+import { Poll } from '../poll/entities/poll.entity';
+import { Report } from '../user/entities/report.entity';
+import { User } from '../user/entities/user.entity';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tag])],
+  imports: [TypeOrmModule.forFeature([Tag, Moderator, Poll, Report, User])],
   controllers: [TagController],
-  providers: [TagService],
+  providers: [TagService, ModeratorService],
   exports: [TagService],
 })
 export class TagModule {}
