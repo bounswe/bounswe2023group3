69effac772bb83bb0390a2742dadaab8e07a7682
Baturhan Akbulut <tahaakbulut@192.168.1.4>
2023-12-23 18:26:08 +0300

    Edit password page connection is fixed.

--- app/mobile/lib/services/passwordEditService.dart
+++ app/mobile/lib/services/passwordEditService.dart
@@ -4,7 +4,7 @@ import 'package:mobile_app/services/apiService.dart';
 
 class PasswordEditService {
 
-  Future<Response> submitPass(String password, String oldPassword) async {
+  Future<Response> submitPass(String password, String confirmPassword, String oldPassword) async {
     const String resetPasswordEndpoint =
         '/user/password';
 
@@ -14,6 +14,7 @@ class PasswordEditService {
       final Response response = await ApiService.dio.put(
         resetPasswordEndpoint,
         data: {'oldPassword': oldPassword,
+          'passwordConfirm': confirmPassword,
           'password': password,},
       );
 
