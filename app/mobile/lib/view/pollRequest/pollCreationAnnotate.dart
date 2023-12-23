55d73b753cbfa6f91caee04c9f2b4ba2ee0d2ef9
faruk.yld <123uzunince123@gmail.com>
2023-12-23 17:27:02 +0300

    refactor code

--- app/mobile/lib/view/pollRequest/pollCreationAnnotate.dart
+++ app/mobile/lib/view/pollRequest/pollCreationAnnotate.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/view/constants.dart';
+import 'package:mobile_app/view/moderatorApproval/sectionHeader.dart';
+import 'package:mobile_app/view/pollRequest/customContainer.dart';
 import 'package:mobile_app/view/pollRequest/pollCreationData.dart';
 
 class PollCreationAnnotate extends StatefulWidget {
@@ -22,37 +25,54 @@ class _PollCreationAnnotateState extends State<PollCreationAnnotate> {
     descrControl = TextEditingController(text: widget.pollInfo.pollDescription);
   }
 
+  final TextStyle style = const TextStyle(fontSize: 18, color: Colors.black);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Annotate Poll'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Text("Title"),
-            TextField(
-              controller: titleControl,
-              readOnly: true,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Annotate Poll'),
+        ),
+        body:
+            // !!! modify here to be a stack
+            Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 15),
+              const SectionHeader(headerText: "Title"),
+              GestureDetector(
+                onTapUp: (details) {
+                  // !!! check the selection
+                  // if selection contains a substring,
+                  // render a floating button button should call a function that
+                  // creates a popup:
+                  // adding annotation for "substring"
+                  // [--inp-fld--]
+                  // [add] // logic is gong to be implemented by me.
+                  // the pop up should be a separate function,
+                  // called with parameters: position, substring
+                  // details.globalPosition
+                },
+                child: TextField(
+                  controller: titleControl,
+                  readOnly: true,
+                  style: style,
+                ),
               ),
-            ),
-            const SizedBox(height: 8), // Adds spacing between the fields
-            const Text("Description"),
-            TextField(
-              controller: descrControl,
-              readOnly: true,
-              style: const TextStyle(
-                fontSize: 18, // Adjusted for description
+              const SizedBox(height: 30), // Adds spacing between the fields
+              const SectionHeader(headerText: "Description"),
+              // !!! do similar gesture detector logic here.
+              TextField(
+                controller: descrControl,
+                readOnly: true,
+                style: style,
+                maxLines: null, // Allows multi-line input
               ),
-              maxLines: null, // Allows multi-line input
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
