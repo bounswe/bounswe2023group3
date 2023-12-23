5490efb3d94964002747a04791819cb57504207d
HaticeSerraHakyemez <h.serra.hakyemez@gmail.com>
2023-12-23 20:28:48 +0300

    Changed upload url

--- app/frontend/src/app/ranks-bar/ranks-bar.component.ts
+++ app/frontend/src/app/ranks-bar/ranks-bar.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-ranks-bar',
   templateUrl: './ranks-bar.component.html',
   styleUrls: ['./ranks-bar.component.css'],
 })
-export class RanksBarComponent {}
+export class RanksBarComponent {
+  @Input() userId!: string
+  ranks!: any
+
+  colors: string[] = [
+    '#f2545b',
+    '#276fbf',
+    '#585191',
+    '#da9f93',
+    '#c5d86d',
+    '#e57f84',
+  ]
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://34.105.66.254:1923/user/' + this.userId).subscribe(
+      (response: any) => {
+        this.ranks = response.badges
+      },
+      (error) => {
+        console.error('Error fetching poll:', error)
+      },
+    )
+  }
+
+  getColor(i: number): string {
+    return this.colors[i % this.colors.length]
+  }
+}
