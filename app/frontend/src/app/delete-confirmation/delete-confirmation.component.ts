d2d9221f2a12223545b1e0a216f1a8bf6e1feb94
beyza-akcinar <beyza.akcinar@hotmail.com>
2023-12-24 18:32:16 +0300

    Delete account popup and its design added

---
+++ app/frontend/src/app/delete-confirmation/delete-confirmation.component.ts
@@ -0,0 +1,19 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-confirmation',
+  templateUrl: './delete-confirmation.component.html',
+  styleUrls: ['./delete-confirmation.component.css']
+})
+export class DeleteConfirmationComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DeleteConfirmationComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string }
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close(false);
+  }
+
+}
